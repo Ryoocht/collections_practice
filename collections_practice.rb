469f74adf4c0c3589c24122afa829c374430c344
Ryochi <ryo1.t.zubu@gmail.com>
2021-09-05 11:55:18 +1000

    Done

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc int_array
+    int_array.sort
+end
 
+def sort_array_desc int_array
+    int_array.sort {|a,b| b <=> a}
+end
+
+def sort_array_char_count str_array
+    str_array.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements str_array
+    str_array.sort {|a,b| a[2] <=> b[1]}
+end
+
+def reverse_array int_array
+    int_array.reverse
+end
+
+def kesha_maker str_array
+    str_array.each {|word| word[2] = "$"}
+end
+
+def find_a str_array
+    str_array.select {|word| word[0] == "a"}
+end
+
+def sum_array int_array
+    int_array.inject(0) {|sum, int| sum + int}
+end
+
+def add_s str_array
+    str_array.each_with_index {|str, index| str << "s" unless index == 1}
+end
